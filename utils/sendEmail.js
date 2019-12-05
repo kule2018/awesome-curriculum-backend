c157149ed4d9830645b724e1498ed7b584658f36
Jacob <lcs1998@vip.qq.com>
2019-12-05 23:55:02 +0800

    :hammer: 修改发送邮件的模板

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -20,7 +20,57 @@ const sendEmail = (to, verificationCode) => {
     to: to, // list of receivers
     subject: '课程助手注册验证码', // Subject line
     // 发送text或者html格式
-    html: `<b>Hello world?</b>${verificationCode}` // html body
+    html: `
+        <div id="contentDiv" onmouseover="getTop().stopPropagation(event);"
+        onclick="getTop().preSwapLink(event, 'html', 'ZC0705-6sa8qKO1peMKHEwRlnQUe9c');"
+        style="position:relative;font-size:14px;height:auto;padding:15px 15px 10px 15px;z-index:1;zoom:1;line-height:1.7;"
+        class="body">
+        <div id="qm_con_body">
+          <div id="mailContentContainer" class="qmbox qm_con_body_content qqmail_webmail_only" style="">
+            <style></style>
+            <div class="WordSection1">
+              <p class="MsoNormal">用户您好：<span lang="EN-US">
+                  <o:p></o:p>
+                </span></p>
+              <p class="MsoNormal"><span lang="EN-US">
+                  <o:p>&nbsp;</o:p>
+                </span></p>
+              <p class="MsoNormal"><span lang="EN-US">&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; </span>感谢您注册课程助手账号，您的验证码是：<span
+                  lang="EN-US">
+                  <o:p></o:p>
+                </span></p>
+              <p class="MsoNormal"><span lang="EN-US">
+                  <o:p>&nbsp;</o:p>
+                </span></p>
+              <p class="MsoNormal" align="center" style="text-align:center"><b><span lang="EN-US"
+  style="font-size:24.0pt">${verificationCode}<o:p></o:p></span></b></p>
+              <p class="MsoNormal" align="center" style="text-align:center"><b><span lang="EN-US" style="font-size:24.0pt">
+                    <o:p>&nbsp;</o:p>
+                  </span></b></p>
+              <p class="MsoNormal" align="left" style="text-align:left">祝好！<span lang="EN-US">
+                  <o:p></o:p>
+                </span></p>
+            </div>
+            <style type="text/css">
+              .qmbox style,
+              .qmbox script,
+              .qmbox head,
+              .qmbox link,
+              .qmbox meta {
+                display: none !important;
+              }
+
+            </style>
+          </div>
+        </div>
+        <style>
+          #mailContentContainer .txt {
+            height: auto;
+          }
+
+        </style>
+      </div>
+    ` // html body
   };
 
   // send mail with defined transport object
