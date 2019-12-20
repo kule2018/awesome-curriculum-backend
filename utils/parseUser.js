498de1303e8d607371b4ab8d48394a39bdd03886
lcs1998 <lcs1998@vip.qq.com>
2019-12-20 16:01:45 +0800

    :sparkles: 新增用户信息验证

---
+++ utils/parseUser.js
@@ -0,0 +1,42 @@
+const tokenUtils = require('./tokenUtils');
+const mysql = require('./mysql');
+
+function cookieToJson(cookie) {
+    let cookieArr = cookie.split(";");
+    let obj = {}
+    cookieArr.forEach((i) => {
+        let arr = i.split("=");
+        obj[arr[0]] = arr[1];
+    });
+    return obj;
+}
+
+const parseUser = async(obj) => {
+    if (!obj) {
+        return;
+    }
+    let s = '';
+    if (typeof obj === 'string') {
+        s = obj;
+    } else if (obj.headers && obj.headers.cookie) {
+        let cookies = cookieToJson(obj.headers.cookie);
+        s = cookies.CurriculumKey;
+    }
+    if (s) {
+        let result = tokenUtils.verifyToken(s);
+        let { id } = result;
+        const queryToken = `
+            select * from token
+            where
+            value = '${s}';
+        `;
+        let response = await mysql.query(queryToken);
+        if (response.length && id && response[0].userId === id) {
+            return id;
+        } else {
+            return ;
+        }
+    }
+}
+
+module.exports = parseUser;
