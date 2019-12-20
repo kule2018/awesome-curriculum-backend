589509bc7570995553084402dcf7ef5793d6608b
lcs1998 <lcs1998@vip.qq.com>
2019-12-20 16:01:10 +0800

    :bug: 修复发送邮件失败的bug，新增上传图片的处理

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ const mysql = require('./utils/mysql');
 const tips = require('./config/response');
 const morgan = require('koa-morgan');
 const fs = require('fs');
+const parseUser = require('./utils/parseUser');
+const path = require('path');
+const koaBody = require('koa-body');
+// 导入WebSocket模块:
+const WebSocket = require('ws');
+// 引用Server类:
+const WebSocketServer = WebSocket.Server;
 
-const accessLogStream = fs.createWriteStream(__dirname + '/access.log',{ flags: 'a' })
+
+const accessLogStream = fs.createWriteStream(__dirname + '/access.log', { flags: 'a' })
 
 
 app.use(cors({
@@ -17,7 +25,12 @@ app.use(cors({
   allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
   allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
 }));
-app.use(morgan('combined', { stream: accessLogStream }))
+app.use(koaBody({
+  multipart: true,
+  formidable: {
+    maxFileSize: 2000 * 1024 * 1024	// 设置上传文件大小最大限制，默认2M
+  }
+}));
 app.use(async (ctx, next) => {
   const { url = '' } = ctx;
   if (!url.includes('registerCode') && !url.includes('register') && !url.includes('login') && !url.includes('logout')) {
@@ -33,7 +46,7 @@ app.use(async (ctx, next) => {
       value = '${token}';
     `;
     let response = await mysql.query(queryToken);
-    if (response.length && id && response[0].userId===id) {
+    if (response.length && id && response[0].userId === id) {
       ctx.state = id;
       await next();
     } else {
@@ -45,4 +58,23 @@ app.use(async (ctx, next) => {
 })
 app.use(bodyParser());
 app.use(router.routes(), router.allowedMethods());
+app.use(morgan('combined', { stream: accessLogStream }))
+
+
 app.listen(3001);
+
+
+let wss = new WebSocketServer({
+  port: 3002
+});
+
+wss.on('connection', async (ws, req) => {
+  parseUser(req).then(response => {
+    const userId = response;
+    console.log(`userId:${userId}的用户登录成功`);
+    if (!userId) {
+      ws.close(4001, 'invalid user');
+    }
+  });
+});
+
